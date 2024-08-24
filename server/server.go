c8877f8423f5d31ca5cdc3b37fef79fdeb0e1329
MatveySTEP <matrasser@mail.ru>
2024-08-24 16:17:57 +0400

    add serv 8080 and 8081, server

--- server/server.go
+++ server/server.go
@@ -1,11 +1,46 @@
 package server
 
-import "load_balancer/client"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io"
+	"load_balancer/client"
+	"net/http"
+)
 
-type SpartimilluServer struct {
+type Server struct {
 	client client.Client
 }
 
-func NewSpartimilluServer(client client.Client) *SpartimilluServer {
-	return &SpartimilluServer{client: client}
+func NewServer(client client.Client) *Server {
+	return &Server{client: client}
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, c *gin.Context) {
+	g := gin.Default()
+
+	g.GET("/", func(c *gin.Context) {
+		fmt.Fprint(c.Writer, fmt.Sprintf("Received request from %s\n", c.Request.RemoteAddr))
+		fmt.Fprint(c.Writer, fmt.Sprintf("%s / %s\n", c.Request.Method, c.Request.Proto))
+		fmt.Fprint(c.Writer, fmt.Sprintf("Host: %s\n", c.Request.Host))
+		fmt.Fprint(c.Writer, fmt.Sprintf("User-Agent: %s\n", c.Request.Header.Get("User-Agent")))
+		fmt.Fprint(c.Writer, fmt.Sprintf("Accept: %+v\n\n", c.Request.Header.Get("Accept")))
+		fmt.Fprint(c.Writer, fmt.Sprintf("Replied with a hello message\n"))
+		fmt.Fprintf(c.Writer, "Hello From Backend Server")
+
+	})
+
+	resp := s.client.ForwardRequest(c)
+
+	fmt.Printf("Response from server: %s %s\n\n", resp.Proto, resp.Status)
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, "Error reading the response body", http.StatusInternalServerError)
+	}
+
+	stringBody := string(body)
+	fmt.Fprint(w, stringBody)
+	fmt.Println(stringBody)
+
 }
